8eb96657c3904f2369396b34b3192f59fdfba5e0
emmatech2 <emmahtech2@gmail.com>
2023-06-03 09:48:09 +0300

    added changes

--- app.js
+++ app.js
@@ -7,48 +7,44 @@ document.querySelector('.theme-toggle-button').addEventListener('click', () => {
 
 //Fetching countries from an API
 async function getCountries() {
-  const res = await fetch(API);
-  const data = await res.json();
+  let res = await fetch(API);
+  let data = await res.json();
  
   return data
 }
 
-const container = document.querySelector('.container')
+let container = document.querySelector('.container')
 container.innerHTML = ''
 
 getCountries().then(data => {
   data.forEach(country => {
 
 
-    const card = document.createElement('div')
+    let card = document.createElement('div')
     card.classList.add('card')
 
-    const image = document.createElement('img')
+    let image = document.createElement('img')
     image.classList.add('card-image')
     image.src = country.flags.png
 
-    const name = document.createElement('h2')
+    let name = document.createElement('h3')
     name.classList.add('name')
     name.textContent = `${country.name.common}`
 
-    const city = document.createElement('p')
+    let city = document.createElement('h3')
     city.classList.add('city')
     city.textContent = `CapitaL City: ${ country.capital}`
 
-    const region = document.createElement('p')
+    let region = document.createElement('h3')
     region.classList.add('region')
     region.textContent =`Region: ${country.region}`
 
-    const populace = document.createElement('p')
+    let populace = document.createElement('h3')
     populace.classList.add('populace')
     populace.textContent = `Population: ${country.population}`
 
 
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
-    btn.textContent = 'Read More'
-
-    card.append(image, name, city, region, populace, btn)
+    card.append(image, name, city, region, populace)
     container.append(card)
 
 
@@ -60,6 +56,7 @@ getCountries().then(data => {
 
 
 searchBar.addEventListener('input', async e => {
+  e.preventDefault()
   const { value } = e.target
   let foundCountry = null
 
@@ -81,28 +78,28 @@ searchBar.addEventListener('input', async e => {
       image.src = country.flags.png
 
 
-      const name = document.createElement('h2');
+      const name = document.createElement('h3');
       name.classList.add('name');
       name.textContent = `Country: ${country.name.official}`;
 
-      const city = document.createElement('p');
+      const city = document.createElement('h3');
       city.classList.add('city');
       city.textContent = `Capital city: ${country.capital}`;
 
-      const region = document.createElement('p');
+      const region = document.createElement('h3');
       region.classList.add('region');
       region.textContent = `Region: ${country.region}`;
 
-      const populace = document.createElement('p');
+      const populace = document.createElement('h3');
       populace.classList.add('populace');
       populace.textContent = `Population: ${country.population}`;
 
-      const btn = document.createElement('button')
-    btn.classList.add('btn')
-    btn.textContent = 'Read More'
+      // const btn = document.createElement('button')
+    // btn.classList.add('btn')
+    //  btn.textContent = 'Read More'
 
 
-      card.append(image, name, city, region, populace, btn)
+      card.append(image, name, city, region, populace)
       container.append(card)
 
 
@@ -129,36 +126,37 @@ searchBar.addEventListener('input', async e => {
         image.classList.add('card-image');
         image.src = foundCountry.flags.svg;  
 
-        const name = document.createElement('h2');
+        const name = document.createElement('h3');
         name.classList.add('name');
         name.textContent = `Country: ${foundCountry.name.official}`;
 
-        const city = document.createElement('p');
-        city.classList.add('city');
+        const city = document.createElement('h3');
+        city.classList.add('city')
         city.textContent = `Capital city: ${foundCountry.capital}`;
 
-        const region = document.createElement('p');
+        const region = document.createElement('h3');
         region.classList.add('region');
         region.textContent = `Region: ${foundCountry.region}`;
 
-        const populace = document.createElement('p');
+        const populace = document.createElement('h3');
         populace.classList.add('populace');
         populace.textContent = `Population: ${foundCountry.population}`;
 
-        const btn = document.createElement('button')
-        btn.classList.add('btn')
-        btn.textContent = 'Read More'
+       
 
-        card.append(image, name, city, region, populace, btn);
+        card.append(image, name, city, region, populace);
         container.append(card);
     }
 };searchBar
 
 
-
-
-
 });
+
+//Adding more info
+
+
+
+
 
 
 
